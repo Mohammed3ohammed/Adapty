ae7b4e1e834cf35472d0efa190b8e1a96d4af157
Hamo <medomedo333hamo@gmail.com>
2023-12-24 23:51:33 +0200

    Upload section DestinationData & Update section Destination

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import '../index.css'
 import Mountain1 from '../images/1.jpg'
-import Mountain2 from '../images/3.jpg'
+import Mountain2 from '../images/6.jpg'
+import Mountain3 from '../images/4.jpg'
+import Mountain4 from '../images/10.jpg'
+import DestinationData from './DestinationData'
 
 const Destination = () => {
   return (
@@ -9,10 +12,11 @@ const Destination = () => {
     <h1>Popular Destination</h1>
     <p>Tours give you the opportunity to see a lot, Within a tiem frame.</p>
 
-    <div className='first-des'>
-        <div className='des-text'>
-            <h2>Taal Volcano, Batangas</h2>
-            <p>One of the most iconic views in Luzon, Mt Taal boasts a 
+   <DestinationData
+   className="first-des"
+   heading="Taal Volcano, Batangas"
+   text="
+   One of the most iconic views in Luzon, Mt Taal boasts a 
               volcano inside a lake inside on island. if you fany a closer
               look, the hike up to the crater is a mere 45 miuutes, and is 
               easy enough for beginners, Guides will assist you most of
@@ -20,14 +24,29 @@ const Destination = () => {
               an active volcano, including volcanic rocks and seteam
               vents. The hike can then unwind with some bulalo before
               heading back home!
-            
-            </p>
-        </div>
-        <div className='image'>
-            <img alt='Img' src={Mountain1} />
-            <img alt='Img' src={Mountain2} />
-        </div>
-    </div>
+   "
+   img1={Mountain1}
+   img2={Mountain2}
+   />
+
+   <DestinationData
+   className="first-des-revers"
+   heading="Mt. Daguldul, Batangas"
+   text="
+   If you'r looking for a hike thats a little more challenging
+   but still good for a beginner mountaineer, check out Mt.
+   Doguldul in San Juan, Batangas. You'll start your hike from 
+   the beach and pass through tropical forest, different rock
+   formations, and smail streams. There's a small store
+   halfway up the trail where you can take a break and drink
+   buko juice, and though the summit itseif may not have the
+   best view, the breeze is fantastic, Once you've made it
+   back down, head straight to the beach for a refreshing,
+   well-deserved swim. 
+   "
+   img1={Mountain3}
+   img2={Mountain4}
+   />
     </div>
   )
 }
